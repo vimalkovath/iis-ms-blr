ccc9f38bd2a622045d284fef13e35f49bfba045e
AYUSH UJJWAL <42496674+ayush1612@users.noreply.github.com>
2019-12-22 11:53:50 +0530

    Update twilioAPI.py

--- 24_AVOCADO/twilioAPI.py
+++ 24_AVOCADO/twilioAPI.py
@@ -11,14 +11,14 @@ def query_example():
     # Download the helper library from https://www.twilio.com/docs/python/install
 	# Your Account Sid and Auth Token from twilio.com/console
 	# DANGER! This is insecure. See http://twil.io/secure
-	account_sid = 'ACd926b871f131815e479f2d86021e5860'
-	auth_token = 'af756f426564ce580aa070f06cd92776'
+	account_sid = 'ur id'
+	auth_token = 'ur id'
 	client = Client(account_sid, auth_token)
 
 	call = client.calls.create(
 	                        url='http://demo.twilio.com/docs/voice.xml',
-	                        to='+918920032466',
-	                        from_='+12029159429'
+	                        to='ur number',
+	                        from_='number from twilio'
 	                    )
 
 	print(call.sid)
